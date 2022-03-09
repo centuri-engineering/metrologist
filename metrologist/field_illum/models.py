9ffe8b2bca23c736e72618259dbd1d99fa4daaba
Youssef <Youssef@IMG-Omega.local>
2022-03-09 11:03:12 +0100

    Correction models.py

--- metrologist/field_illum/models.py
+++ metrologist/field_illum/models.py
@@ -3,7 +3,6 @@
 import datetime as dt
 import logging
 
-
 from metrologist.database import (
     Column,
     PkModel,
@@ -17,77 +16,67 @@ log = logging.getLogger(__name__)
 import homogeneity_module as homo
 import common_module as cm
 
-# sqlalchemy() imported as db (db = SQLAlchemy())
-"""written previously
-class Homogeneity(PkModel):
-    #__tablename__ = "homogeneities"
-    #top_left = Column(db.BigInter())
+"""
+sqlalchemy() imported as db (db = SQLAlchemy())
+when imported metrologist.database
+"""
+
+metadata = db.MetaData()
+
+"""
+Rk: missing: microscopy_info_table defined from microscopes folder
+as it's supposed to be part of "microscopes" dir rather than
+homogeneity. 
 """
 
 
-class Homogeneity:
+class Homogeneity(PkModel):
+    """
+    Homogeneity class refers to homogeneity report generating
+    from a given image:
+        1. norm_intensity_data: np.array used to generate the normalized
+        intensity profile of the image. It's used to generate the corresponding
+        image.
+        2. max_region_table: dataframe enclosing information about the max
+        intensity area of the given image.
+        3. intensity_plot_data: np.array used to generate the intensity plot of
+        the mid horizontal, mid vertical and the two diagonal lines of the image.
+        4. profile_stat_table: dataframe showing the intensity values of 9
+        specific pixels and their ratio over the maximum intensity value of
+        the array.
+    """
 
     __tablename__ = "homogeneities"
 
-    profile_stat_table = db.Table(
-                                       name="profile_stat_table", metadata=metadata,
-                                       db.Column(name="location", type_=db.Text, nullable=False),
-                                       db.Column(name="intensity", type_=db.Float, nullable=False),
-                                       db.Column(name="intensity_relative_to_max", type_=db.Float, nullable=False)
-                                       )
+    # user_id
+    user_id = Column(db.ForeignKey("users.id"), nullable=False)
 
-    max_region_table = db.Table(
-                                       name="max_region_table", metadata=metadata,
-                                       db.Column(name="nb_pixels", type_=db.Integer, nullable=False),
-                                       db.Column(name="center_of_mass", type_=db.ARRAY(Integer), nullable=False),
-                                       db.Column(name="max_intensity", type_=db.Integer, nullable=False))
+    # microscope
+    microscope_id = Column(db.ForeignKey("microscopes.id"), nullable=False)
 
-    intensity_plot_data = db.Table(
-                                        name="intensity_plot_data", metadata=metadata,
-                                        db.Column(name="x_axis_V_seg", type_=db.Float, nullable=False),
-                                        db.Column(name="y_axis_V_seg", type_=db.Float, nullable=False),
-                                        db.Column(name="x_axis_H_seg", type_=db.Float, nullable=False),
-                                        db.Column(name="y_axis_H_seg", type_=db.Float, nullable=False),
-                                        db.Column(name="x_axis_diagUD", type_=db.Float, nullable=False),
-                                        db.Column(name="y_axis_diagUD", type_=db.Float, nullable=False),
-                                        db.Column(name="x_axis_diagDU", type_=db.Float, nullable=False),
-                                        db.Column(name="y_axis_diagDU", type_=db.Float, nullable=False))
+    # datetime
+    created_at = Column(db.DateTime, nullable=True, default=dt.datetime.utcnow)
 
-    # intensity_plot_data = Array
-                                        
-                                        
-    
-    
-    def image_array(self, path):
-        return cm.get_images_from_multi_tiff(path)[0]
+    # max_region_table
+    max_region_table_nb_pixels = Column(db.Integer, nullable=False)
+    max_region_table_center_of_mass = Column(db.ARRAY(Integer), nullable=False)
+    max_region_table_max_intensity = Column(db.Integer, nullable=False)
 
-    image_array_ = cm.get_images_from_multi_tiff(path)[0]
+    # intensity_plot_data
+    intensity_plot_data_x_axis_V_seg = Column(db.ARRAY(Float), nullable=False)
+    intensity_plot_data_y_axis_V_seg = Column(db.ARRAY(Float), nullable=False)
+    intensity_plot_data_x_axis_H_seg = Column(db.ARRAY(Float), nullable=False)
+    intensity_plot_data_y_axis_H_seg = Column(db.ARRAY(Float), nullable=False)
+    intensity_plot_data_x_axis_diagUD = Column(db.ARRAY(Float), nullable=False)
+    intensity_plot_data_y_axis_diagUD = Column(db.ARRAY(Float), nullable=False)
+    intensity_plot_data_x_axis_diagDU = Column(db.ARRAY(Float), nullable=False)
+    intensity_plot_data_y_axis_diagDU = Column(db.ARRAY(Float), nullable=False)
 
-    def norm_intensity_profile(self,  img=image_array_):
-        norm_intensity_profile_ = homo.get_norm_intensity_profile(img)
-        return norm_intensity_profile_
-
-    def max_intensity_region_table(self,  img=image_array_):
-        max_intensity_region_table_ = homo.get_max_intensity_region_table(img)
-        return max_intensity_region_table_
-
-    def intensity_plot(self,  img=image_array_):
-        intensity_plot_ = homo.intensity_plot(img)
-        return intensity_plot_
-
-    def profile_stat_table(self, img=image_array_):
-        profile_stat_table_ = homo.get_profile_statistics_table(img)
-        return profile_stat_table_
+    # profile_stat_table 
+    profile_stat_table_location = Column(db.String(128), nullable=False)
+    profile_stat_table_intensity = Column(db.Float, nullable=False)
+    profile_stat_table_intensity_relative_to_max = Column(db.Float, nullable=False)
 
     
-
-
-"""
-Final attributes :
-Homogeneity.intensity_plot
-Homogeneity.max_region_table
-Homogeneity.norm_intensity_profile
-Homogeneity.profile_state_table
-
-missing: microscopy_info_table defined from microscopes folder
-"""
+    # norm_intensity_data
+    norm_intensity_data = Column(db.ARRAY(Float, dimensions=2)) 
